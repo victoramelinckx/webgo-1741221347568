d367e4e6e9ec74c08fbdb564ce90d7486099d564
Victor Amelinckx <68256527+victoramelinckx@users.noreply.github.com>
2025-03-05 19:35:53 -0500

    Initial commit from WebGo

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+        content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+        theme: {
+          extend: {
+            colors: {
+              primary: '#000000',
+              secondary: '#000000'
+            }
+          }
+        },
+        plugins: []
+      }
